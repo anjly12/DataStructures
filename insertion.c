86175e4a57ee4d2daa84e2cc355cc586a0f9be84
anjly12 <anjalymanala02@gmail.com>
2024-09-23 14:09:50 +0530

    Add files via upload

--- insertion.c
+++ insertion.c
@@ -4,58 +4,33 @@ int main()
  int n,i,key,j,t=0;
  printf("Enter the number of elements:");
  scanf("%d",&n);
- t++;
  int a[n];
- t++;
  printf("Enter %d elements:\n",n);
- t++;
  for(i=0;i<n;i++)
  {
   scanf("%d",&a[i]);
-  t++;
-  t++;
  }
  printf("Orginal array:\n");
- t++;
  for(i=0;i<n;i++)
  {
   printf("%d\t",a[i]);
-  t++;
-  t++;
  }
  printf("\n");
- t++;
  for(i=0;i<n;i++)
  {
   key=a[i];
   j=i-1;
-  t++;
-  t++;
 while (j>=0 && a[j]>key)
  { 
   a[j+1]=a[j];
   j--;
-  t++;
-  t++;
  }
  a[j+1]=key;
- t++;
  }
  printf("The sorted array is:\n");
- t++;
  for (i=0;i<n;i++)
  {
   printf("%d\t",a[i]);
-  t++;
  }
- t++;
  printf("\n");
- t++;
- printf("Space complexity=%d\n",((5*4)+4*n));
-t++;
-t++;
-printf("Time complexity=%d\n",t);
 }
- 
- 
- 
